1946a6b453a3f32176a90e7620ba92d486b9caa8
Cesar Villalobos <cvillalo@bancolombia.com.co>
2025-03-19 22:17:40 -0500

    Se continua en proyecto

--- proyecto/src/app/components/alert/alert.component.ts
+++ proyecto/src/app/components/alert/alert.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-alert',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./alert.component.css']
 })
 export class AlertComponent {
+  constructor(private router:Router){
+
+  }
   menuItemsAlert = ["Inicio", "Necesidades", "Productos y Servicios", "Educación Financiera", "Trámites Digitales", "Sucursal Virtual Personas"]
+  gologin(){
+    this.router.navigate(['/login'])
+  }
 }
